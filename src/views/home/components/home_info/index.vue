2ac314e8ef6144ac996ef14bf1f7b9d4f4fffd95
xhn000406 <1127835255@qq.com>
2022-09-03 00:24:29 +0800

    perfect home_title

--- src/views/home/components/home_info/index.vue
+++ src/views/home/components/home_info/index.vue
@@ -1,13 +1,49 @@
 <template>
-  <div id="home_info"></div>
+  <div id="home_info">
+    <div class="author"><img src="" alt="" /></div>
+    <template v-for="item in titleName" :key="item.title">
+      <div class="title">
+        <div>{{ item.title }}</div>
+        :
+        <div>{{ item.value }}</div>
+      </div>
+    </template>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const titleName = ref([
+  {
+    title: '姓名',
+    value: '徐浩楠'
+  },
+  {
+    title: '目标',
+    value: '找份实习工作'
+  }
+])
+</script>
 
 <style scoped>
 #home_info {
   width: 300px;
   height: 300px;
   background-color: red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.author {
+  width: 140px;
+  height: 140px;
+  background-color: pink;
+  border-radius: 50%;
+}
+.title {
+  display: flex;
+  font-size: 14px;
+  margin-top: 8px;
 }
 </style>
